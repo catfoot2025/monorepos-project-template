44391a246c87a36f2925f6b1492f2c8d90aa960d
catfoot2025 <catfoot@126.com>
2025-08-21 13:32:06 +0800

    chore(project): 移除cz-git适配器及其配置文件

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,8 +1,12 @@
 import { execSync } from 'child_process'
 import fg from 'fast-glob'
 
+// 以同步方式获取指定目录下所有的目录，扁平化后，以数组的形式返回
+// ['src/alarm', 'src/device']
 const getPackages = (packagePath) =>
   fg.sync('*', { cwd: packagePath, onlyDirectories: true })
+
+console.log(getPackages('.'))
 
 const scopes = [
   ...getPackages('packages'),
@@ -25,10 +29,17 @@ const scopes = [
   'deps',
 ]
 
+// execSync同步执行 shell 命令的方法
+// Git 命令，用于获取仓库状态的机器可读格式**​（Porcelain Format）。
+// 与常规 git status 相比，其输出更结构化（如用 M 表示修改、A 表示新增、?? 表示未跟踪等），适合脚本解析。
 const gitStatus = execSync('git status --porcelain || true')
+  // 将返回的Buffer格式转换为string
   .toString()
+  // 去除掉首尾的空白字符，包括空格、tab、换行符\n等
   .trim()
+  // 将字符串按换行符分割成数组
   .split('\n')
+console.log(gitStatus)
 
 const scopeComplete = gitStatus
   .find((r) => ~r.indexOf('M  packages'))
